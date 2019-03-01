60684bf940140d0245e29031cda74b99542b1287
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2019-03-01 13:56:54 -0800

    Fix setup.py

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,26 @@
-#!/bin/bash
+# #!/bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade
+# # sudo nano /etc/hostname
+# # sudo nano /etc/hosts
+# # dpkg-reconfigure tzdata
+
+sudo apt-get -y update
+
+sudo apt-get -y upgrade
 
 sudo apt-get -y install glances git build-essential \
   python-dev python-pip python3-pip \
-  pimoroni python3-blinkt python-blinkt
+  python-smbus python3-smbus \
+  python-numpy python3-numpy \
+  python-pil python3-pil \
+  python-psutil python3-psutil \
+  python-tweepy python3-tweepy \
+  python-blinkt python3-blinkt pimoroni \
+  python-scrollphathd python3-scrollphathd \
 
-sudo pip install RPi.GPIO requests
-sudo pip3 install adafruit-circuitpython-ssd1306
-sudo pip3 install RPI.GPIO
-sudo pip3 install adafruit-blinka
+# # sudo pip install RPi.GPIO requests
+# # sudo pip3 install adafruit-circuitpython-ssd1306
+# # sudo pip3 install RPI.GPIO
+# # sudo pip3 install adafruit-blinka
+
+# # curl https://get.pimoroni.com/scrollphathd | bash
