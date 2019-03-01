ba58fbc6cf8739af149f893c838056d1e6daf1b6
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2019-03-01 14:08:57 -0800

    Add echo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,10 +4,13 @@
 # # sudo nano /etc/hosts
 # # dpkg-reconfigure tzdata
 
+echo "> UPDATE"
 sudo apt-get -y update
 
+echo "> UPGRADE"
 sudo apt-get -y upgrade
 
+echo "> INSTALL"
 sudo apt-get -y install glances git build-essential \
   i2c-tools libi2c-dev \
   python-dev python-pip python3-pip \
@@ -19,6 +22,7 @@ sudo apt-get -y install glances git build-essential \
   python-blinkt python3-blinkt pimoroni \
   python-scrollphathd python3-scrollphathd \
 
+echo "> PIP"
 sudo pip install RPi.GPIO requests
 sudo pip3 install adafruit-circuitpython-ssd1306
 sudo pip3 install RPI.GPIO requests
